6afafafd49c52a2d9b5aafd15db50dc9a904141f
frbi-zvnt <francesco.bianchi.zvnt@outlook.com>
2021-08-07 19:37:57 +0200

    set device (anad some funcs

--- meig-system/public/js/global_data.js
+++ meig-system/public/js/global_data.js
@@ -23,6 +23,51 @@ const init_sections = [{
 }, ]
 
 
+const init_devices = [{
+    label: "DEVICES",
+    id: genId(),
+    children: [{
+        label: "GROUP",
+        type: "group",
+        id: genId(),
+        children: [],
+    }, ],
+}, ]
+
+
+const create_group = (name) => {
+    return {
+        label: name,
+        id: genId(),
+        type: 'group',
+        children: []
+    }
+}
+
+
+const create_device = (name) => {
+    return {
+        label: name,
+        id: genId(),
+        type: 'device',
+        children: [{
+            label: "I/O",
+            id: genId(),
+            type: 'io',
+            children: []
+        }, {
+            label: "Parameters",
+            id: genId(),
+            type: 'parameter',
+            children: []
+        }]
+    }
+}
+
+
 export const global_data = {
-    INIT_SECTIONS: init_sections
+    INIT_SECTIONS: init_sections,
+    INIT_DEVICES: init_devices,
+    create_group: create_group,
+    create_device: create_device
 }
