b56505d181c765b432d9ec7ac7fe3948a98168e0
franeum <elettrochuck@gmail.com>
2021-08-11 19:05:20 +0200

    add preset page (rendered) prototype

--- meig-system/controllers/presetController.js
+++ meig-system/controllers/presetController.js
@@ -1,16 +1,31 @@
 // section
-
-
-
-
+const Max = require('max-api')
+const Tree = require('tree-model')
+const tree = new Tree()
+const utils = require('./utils')
 
 
 
 
 /*************************************************************
- * GET SECTION PAGE && TREE
+ * GET PRESET
  ************************************************************/
 
 exports.get_preset = (req, res) => {
-    res.render('presets')
+    Max.getDict('devices')
+        .then(data => {
+            const root = tree.parse(data)
+            root.all(node => {
+                if (node.model.type == 'parameter_name')
+                    node.model.path = utils.findPathName(node)
+            })
+            const nodes = utils.findAllByType(root, 'parameter_name')
+            res.render('presets', {
+                presets: nodes
+            })
+        })
+        .catch(err => {
+            res.send(err)
+        })
+        // res.render('presets')
 }
